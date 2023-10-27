22141673fa1991eaec2dc85df78c6af9709f5ad4
SofaUa <130164330+SofaUa@users.noreply.github.com>
2023-10-27 19:42:22 +0200

    add files

---
+++ app.js
@@ -0,0 +1,13 @@
+let burger = document.querySelector('.burger')
+let menu = document.querySelector('.navigation ul')
+let navigation = document.querySelector('.navigation')
+burger.addEventListener('click', function() {
+   navigation.classList.toggle('close')
+})
+
+
+
+
+
+
+
